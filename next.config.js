f1c4be11e00ee75796f9ff5db3ce820083112489
SatiraCloset <satirascloset.website@gmail.com>
2025-04-02 20:37:00 -0400

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['localhost', 'satiras-closet.vercel.app'],
-  },
-  experimental: {
-    serverActions: true,
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig 
